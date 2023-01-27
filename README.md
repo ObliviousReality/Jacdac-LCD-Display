dd12ffec87f7d0fea428349bf9bed1602a0679e3
Tom <t.litherland@lancaster.ac.uk>
2023-01-27 14:16:39 +0000

    Updated Readme and added comments.

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# Jacdac-LCD-Display
+# Microbit Arcade Programming using MKC
+
+## Requirements
+
+- Node.js
+
+## Installation
+
+To install MKC:
+
+`npm install -g makecode`
+
+## Setup
+
+In an empty folder, run
+
+`mkc install arcade`
+
+There are other template options, notably `microbit` for standard Microbit programming, or `maker-jacdac-brain-rp2040` for the Jacdac RP2040 module. A full list of templates can be generated with the command `mkc help init`.
+
+This command creates the files for programming the microbit in the current directory.
+
+The file `main.ts` can be used to write code for the Microbit.
+
+## Build
+
+To build the code, run
+
+`mkc`
+
+in the root directory. This creates a .hex file within the `built` folder. In my case MKC automatically chose the n3 build variant, which was exactly what I needed. This can be edited in the file `mkc.json`, by adding the line:
+
+`"hwVariant": "[variant]]"`
+
+A list of variants can be generated with the command `mkc --hw help`.
+
+If the Microbit is currently connected via USB, the command
+
+`mkc -d`
+
+can be ran, which automatically flashes the .hex file to the Microbit after build.
+
+## Dependencies
+
+Extra dependencies can be added using the command
+
+`mkc add [...]`
+
+The dependencies can either be GitHub links or, in the case of Jacdac, just the name of the module required.
+
+To add Jacdac support:
+
+`mkc add jacdac`
+
+Adds the Jacdac MakeCode extension; necessary for all Jacdac modules.
+
+To add a Jacdac module:
+
+`mkc add jacdac-button`
+
+OR
+
+`mkc add https://github.com/microsoft/pxt-jacdac/button`
+
+## See Also
+
+[MKC Documentation](https://www.npmjs.com/package/makecode)
+
+[MKC GitHub](https://github.com/microsoft/pxt-mkc)
--- main.ts
+++ main.ts
@@ -1,40 +1,51 @@
-let picture = image.create(160, 120)
+let picture = image.create(screen.width, screen.height) // Creates an editable bitmap image the size of the screen.
 
+/**
+ * Sets all pixels to black on the screen
+ */
 function clear() {
-    for (let x = 0; x < 160; x++) {
-        for (let y = 0; y < 120; y++) {
+    for (let x = 0; x < screen.width; x++) {
+        for (let y = 0; y < screen.height; y++) {
             picture.setPixel(x, y, 0);
         }
     }
 }
-
+/**
+ * Creates a sprite from the image. This is necessary to actually show the image produced. This only has to be done once; further additions to the image will be seen, any changes before this is called however cannot be seen on the display.
+ */
 function draw() {
     let test = sprites.create(picture);
 }
 
-picture.fillRect(0, 0, 100, 100, 2)
-picture.setPixel(50, 50, 1)
-picture.drawCircle(50, 50, 10, 5)
-
-
+picture.fillRect(0, 0, 100, 100, 2) // Fileld rectangle.
+picture.setPixel(50, 50, 1) // Sets the value of a single pixel.
+picture.drawCircle(50, 50, 10, 5) // Empty circle
 draw();
+
+// Press A to clear the screen.
 controller.A.onEvent(ControllerButtonEvent.Pressed, () => { clear(); })
+
+// Press B to draw a filled red rectangle.
 controller.B.onEvent(ControllerButtonEvent.Pressed, () => {
     picture.fillRect(0, 0, 100, 100, 2);
 })
 
+// Press UP to generate a random empty rectangle.
 controller.up.onEvent(ControllerButtonEvent.Pressed, () => {
     picture.drawRect(randint(0, 100), randint(0, 100), randint(0, 100), randint(0, 100), randint(0, 16));
 });
 
+// Press DOWN to generate a random empty circle.
 controller.down.onEvent(ControllerButtonEvent.Pressed, () => {
     picture.drawCircle(randint(0, screen.width), randint(0, screen.height), randint(0, 50), randint(0, 16));
 });
 
+//Press LEFT to generate a random line.
 controller.left.onEvent(ControllerButtonEvent.Pressed, () => {
     picture.drawLine(randint(0, screen.width), randint(0, screen.height), randint(0, screen.width), randint(0, screen.height), randint(0, 16));
 });
 
+// Press RIGHT to generate a random pixel.
 controller.right.onEvent(ControllerButtonEvent.Pressed, () => {
     picture.setPixel(randint(0, screen.width), randint(0, screen.height), randint(0, 16));
 });
